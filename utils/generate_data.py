6c10b7fdda04838fb7a311ff7004c551dae79a4a
Gerônimo Lemos <ggzlemos@inf.ufpel.edu.br>
2024-02-28 23:36:26 -0300

    feat: add day, month and year to input file

--- utils/generate_data.py
+++ utils/generate_data.py
@@ -1,4 +1,5 @@
 import os 
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from utils.constants import TARGET_FILE_NAME, TARGET_COLUMN, INPUT_COLUMNS
@@ -17,13 +18,19 @@ def generate_unique_file(filename: str, path: str) -> None:
     print(f"Extracting files from {path}...")
     
     for subdir, dirs, files in os.walk(path):
+        split_string_ano = subdir.split('/')
         for file in files:                
             file_path = os.path.join(subdir, file)
-            file_content = pd.read_csv(file_path)
 
-            if TARGET_FILE_NAME in file_path:          
+            file_content = pd.read_csv(file_path) 
+
+            if TARGET_FILE_NAME in file_path:
+
                 y = pd.concat([y, file_content])
             else:
+                file_content['dia'] = split_string_ano[-1][2:]
+                file_content['mes'] = split_string_ano[-1][:2]
+                file_content['ano'] = split_string_ano[-2]
                 X = pd.concat([X, file_content])
 
     #add the columns names to both dataframes
@@ -43,3 +50,18 @@ def generate_unique_file(filename: str, path: str) -> None:
 
     data = data.drop(columns=['index'])
     data.to_csv(f"{os.getcwd()}/data/{filename}", index=False)
+
+def expand_data(dataframe: pd.DataFrame, column: str, variable: str) -> dict:
+    """Expandas the column shape of the data to an actual n X m grid
+
+    Keywords arguments:
+    dataframe -- data to be expanded
+    column -- column that will be used to expand the dataframe
+    variable -- column (variable) that will be part of the grid
+    """
+    result = {}
+    for i in dataframe[variable].unique():
+        coluna_resutl = dataframe.loc[dataframe[variable] == i][column].to_numpy()
+        result[int(i)] = coluna_resutl
+
+    return result
